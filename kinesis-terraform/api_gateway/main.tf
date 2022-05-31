574bd859f6434e26004f25e477623952413daa77
hengsgg <dnflekf2748@gmail.com>
2022-05-31 15:33:32 +0900

    Update api gateway delivery code

--- kinesis-terraform/api_gateway/main.tf
+++ kinesis-terraform/api_gateway/main.tf
@@ -122,14 +122,14 @@ resource "aws_api_gateway_resource" "driver" {
 
 resource "aws_api_gateway_resource" "resource_user" {
   path_part   = "{id}"
-  parent_id   = aws_api_gateway_resource.driver.id
+  parent_id   = aws_api_gateway_resource.driver.id  
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 }
 
 resource "aws_api_gateway_method" "method_user" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.resource_user.id
-  http_method   = "GET"
+  http_method   = "GET" 
   authorization = "NONE"
 }
 
