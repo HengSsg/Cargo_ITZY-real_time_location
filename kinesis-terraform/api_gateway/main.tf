5a00f778d2c34aa64322cdc2724cf01fedee9bad
hengsgg <dnflekf2748@gmail.com>
2022-05-31 15:10:40 +0900

    Update api gateway delivery code

--- kinesis-terraform/api_gateway/main.tf
+++ kinesis-terraform/api_gateway/main.tf
@@ -126,7 +126,7 @@ resource "aws_api_gateway_resource" "resource_user" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "method_user" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.resource_user.id
   http_method   = "GET"
@@ -136,7 +136,7 @@ resource "aws_api_gateway_method" "method" {
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.resource_user.id
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.method_user.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.module.lambda_function.lambda_function_
